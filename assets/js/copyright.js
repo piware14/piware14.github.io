db3fc6c45d73ca82739599767e12e87517277457
oxmc <oxmc7769@gmail.com>
2021-12-08 23:21:01 -0800

    update site

---
+++ assets/js/copyright.js
@@ -0,0 +1,4 @@
+var copyright = document.getElementById('copyright');
+var currentyear = new Date().getFullYear();
+
+copyright.innerHTML = `© Copyright ${currentyear} The Pi-Ware-Team. All rights reserved.`;

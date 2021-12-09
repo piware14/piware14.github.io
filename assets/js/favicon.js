55be2a0b504851e134f0fa0eeb67b2e26ea8321f
oxmc <67136658+oxmc@users.noreply.github.com>
2021-12-09 15:36:13 -0800

    Update favicon.js

--- assets/js/favicon.js
+++ assets/js/favicon.js
@@ -12,7 +12,29 @@ if (!link) {
 
 if (currentMonth == "12") {
   if (currentDay == "25") {
-    alert("Merry Christmas and Happy Holidays from the pi-ware team!");
+    /*alert("Merry Christmas and Happy Holidays from the pi-ware team!");*/
+    iziToast.show({
+      id: 'haduken',
+      theme: 'dark',
+      icon: 'icon-contacts',
+      title: 'Merry Chrsitmas!',
+      displayMode: 2,
+      message: 'Happy Holidays from the pi-ware team!',
+      position: 'Center',
+      transitionIn: 'flipInX',
+      transitionOut: 'flipOutX',
+      progressBarColor: 'rgb(0, 255, 184)',
+      image: '../images/logo-full-chrstmas.jpg',
+      imageWidth: 70,
+      layout: 2,
+      onClosing: function(){
+        console.info('onClosing');
+      },
+      onClosed: function(instance, toast, closedBy){
+        console.info('Closed | closedBy: ' + closedBy);
+      },
+      iconColor: 'rgb(0, 255, 184)'
+    });
   };
   link.href = 'https://pi-ware.ml/assets/images/logo-full-christmas.png';
 };

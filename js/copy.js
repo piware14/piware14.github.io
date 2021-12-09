077c169f9e8f4966ee30542f8065f57c1f7e78cb
oxmc <67136658+oxmc@users.noreply.github.com>
2021-12-08 23:09:24 -0800

    Update copy.js

--- js/copy.js
+++ js/copy.js
@@ -1,9 +1,11 @@
 function copyToClipboard(element) {
-  var $temp = $("<input>");
-  $("body").append($temp);
-  $temp.val($(element).text()).select();
-  document.execCommand("copy");
-  $temp.remove();
+  console.log(`Element: ${element}`);
+  var copytext = document.getElementById(element);
+  console.log(`Text to copy: ${copytext.value}`);
+  copytext.select();
+  copytext.setSelectionRange(0, 99999); /* For mobile devices */
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copytext.value);
   var tooltip = document.getElementById(`${element}-tooltip`);
   tooltip.innerHTML = "Copied!";
   setTimeout(function() {tooltip.innerHTML = "Copy to clipboard";},1000);

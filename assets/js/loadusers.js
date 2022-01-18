8db527e716280f9131e398b1493fb08d97c05eda
oxmc <67136658+oxmc@users.noreply.github.com>
2022-01-17 19:07:32 -0800

    Update loadusers.js

--- assets/js/loadusers.js
+++ assets/js/loadusers.js
@@ -1,15 +1,24 @@
-fetch('https://pi-ware.ml/assets/team.json').then(function (response) {
-  return response.json();
-}).then(function (data) {
-  //console.log(data)
-  appendData(data);
-}).catch(function (err) {
-  console.log('error: ' + err);
-});
+/* Functions */
+async function GetLogo(user) {
+  const res = await axios.get(`https://api.github.com/users/${user}`);
+  return res.data.avatar_url;
+};
 
-function appendData(data) {
+async function GetTeam() {
+  const res = await axios.get('https://pi-ware.ml/assets/team.json');
+  return res.data;
+};
+
+async function appendData(data) {
   var count = 1;
   for (var i = 0; i < data.length; i++) {
+    /* Set variables */
+    var name = data[i].name;
+    var role = data[i].role;
+    var logo = data[i].logourl;
+    var desc = data[i].desc;
+    
+    /* Get elements */
     var app = 'AppList'+count
     var mainContainer = document.getElementById(app);
 
@@ -21,17 +30,30 @@ function appendData(data) {
     var applistname = document.createElement('h1');
     var applistdesc = document.createElement('p');
     var div2 = document.createElement('div');
+    var br = document.createElement('br');
+      
+    if (data[i].logourl == "github") {
+      var logogh = await GetLogo(data[i].name);
+      var logo = logogh;
+      console.log(`using github logo for ${data[i].name}`);
+    } else if (data[i].logourl == "none" || null) {
+      console.log(`using placeholder for ${data[i].name}`);
+      var logo = "https://oxmc.xyz/assets/img/placeholder.png";
+    } else {
+      console.log(`using custom logo for ${data[i].name}`);
+    };
+    //console.log(logo);
 
     /* Add classes, images, title and description */
-    applist.className += 'col';
+    applist.className += 'col card-deck';
     applist1.className += 'card shadow-sm';
     applistimg.className += 'card-img-top';
-    applistimg.src=data[i].logourl;
+    applistimg.src=logo;
     applistdescdiv.className += 'card-body';
     applistdesc.className += 'card-text';
-    applistname.innerHTML = data[i].name;
+    applistname.innerHTML = name;
     applistname.className += 'card-text';
-    applistdesc.innerHTML = data[i].desc;
+    applistdesc.innerHTML = desc;
     div2.className += 'd-flex justify-content-between align-items-center';
 
     /* Add to main html */
@@ -40,6 +62,7 @@ function appendData(data) {
     applistdescdiv.appendChild(applistdesc);
     applist1.appendChild(applistimg);
     applist1.appendChild(applistdescdiv);
+    applist1.appendChild(br);
     applist.appendChild(applist1);
     mainContainer.appendChild(applist);
 
@@ -50,3 +73,11 @@ function appendData(data) {
     };
   };
 };
+
+async function start() {
+  var teamjson = await GetTeam();
+  appendData(teamjson);
+};
+
+/* Main */
+start();

3b3e594a4c154fa0c67b39cda43d234ef3216a93
oxmc <67136658+oxmc@users.noreply.github.com>
2022-04-22 10:00:09 -0700

    Create anniversary.js

---
+++ assets/js/anniversary.js
@@ -0,0 +1,30 @@
+var cfh = document.getElementById("confetti-holder");
+const date = new Date();
+const currentMonth = date.getMonth() + 1;
+const currentDay = date.getDate();
+
+if (currentMonth == "4") {
+  if (currentDay == "24") {
+    iziToast.show({
+      id: 'anniversary',
+      theme: 'dark',
+      icon: 'icon-contacts',
+      title: 'Happy anniversary to pi-ware!',
+      displayMode: 2,
+      message: 'Todays pi-wares anniversary!',
+      position: 'center',
+      transitionIn: 'flipInX',
+      transitionOut: 'flipOutX',
+      progressBarColor: 'rgb(0, 255, 184)',
+      image: '../assets/images/logo-full.png',
+      imageWidth: 70,
+      layout: 2,
+      iconColor: 'rgb(0, 255, 184)'
+    });
+    for (let i = 0; i < 25; i++) {
+      let div = document.createElement("div");
+      div.classList = "confetti";
+      cfh.appendChild(div);
+    };
+  };
+};

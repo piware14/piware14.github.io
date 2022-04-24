0f5f36157e19669c7dd11515939db9c296fe34e2
oxmc <67136658+oxmc@users.noreply.github.com>
2022-04-24 02:53:07 -0400

    Update date.js

--- assets/js/date.js
+++ assets/js/date.js
@@ -1,3 +1,8 @@
 const date = new Date();
+const currentDay = date.getFullYear();
 const currentMonth = date.getMonth() + 1;
 const currentDay = date.getDate();
+function yearssince(year) {
+  var ageInMilliseconds = new Date() - new Date(year+"-01-01");
+  return Math.floor(ageInMilliseconds/1000/60/60/24/365); // convert to years
+};
